1dc99099ad775810ed7e7e6c6eb4036aa37f71b7
soundhi2097 <soundhi.2097@gmail.com>
2026-06-10 21:20:49 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,5 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
          System.out.println ("Hello everyone from demo java here");
-         System.out.println ("Used webhook");
     }
 }
