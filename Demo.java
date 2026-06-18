7d22098c15b097c1c78c29f94d953cb6f06530ec
soundhi2097 <soundhi.2097@gmail.com>
2026-06-18 18:59:07 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
          System.out.println ("sample java code");
+         System.out.println ("Changes made for POLL scm");
     }
 }
