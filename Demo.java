007ef5f726c2c9f5d6ea5240b3f9cdef423ee22c
soundhi2097 <soundhi.2097@gmail.com>
2026-07-07 19:31:35 +0530

    Update output message in Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
-         System.out.println ("sample java code11");
+         System.out.println ("sample java code12");
          System.out.println ("Changes made for webhook");
     }
 }
